946b3b915f2e7b22af7249e6591d912b65ed89f6
Murat Kaan Meral <muratkaanmeral@gmail.com>
2018-12-16 13:19:59 +0100

    Added howto view

--- tavla/routes/index.js
+++ tavla/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var ejs = require('ejs');
+/* GET game page */
+// TODO: Game Variables
+router.get('/game', function(req, res, next) {
+    res.render('game',
+        {
+
+        });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('splash', { defaultImage: '/images/EarthCountryball.png' });
+    let howto = ejs.render('howto', {});
+
+    res.render('splash',
+      {
+          defaultImage: '/images/EarthCountryball.png',
+          games: 0,
+          users: 0,
+          avgWait: 0,
+          howto: howto,
+      });
 });
 
 module.exports = router;
