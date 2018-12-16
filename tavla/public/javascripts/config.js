08fbd966e1200f31f1ec5b790ab3b5616ac9d799
Murat Kaan Meral <muratkaanmeral@gmail.com>
2018-12-16 16:51:31 +0100

    Added countryball list

--- tavla/public/javascripts/config.js
+++ tavla/public/javascripts/config.js
@@ -3,5 +3,13 @@
 
     exports.WEB_SOCKET_URL = "ws://localhost:3000"; /* WebSocket URL */
     exports.DEFAULT_BOARD = [6,6,6,6,6,8,8,8,13,13,13,13,13,24,24];
+    exports.COUNTRYBALLS=[
+        "ar.png","br.png","de.png","gb.png",
+        "it.png","no.png","ro.png","tr.png",
+        "at.png","ca.png","es.png","in.png",
+        "jp.png","pl.png","ru.png","us.png",
+        "be.png","cn.png","fr.png","is.png",
+        "nl.png","pt.png","se.png",
+    ]
 
 }(typeof exports === "undefined" ? this.Setup = {} : exports));
