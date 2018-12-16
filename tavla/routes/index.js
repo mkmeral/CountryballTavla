aa9974356286a062472cc4e6498ef366d8512ae2
Sina Sen <sinasen2000@hotmail.com>
2018-12-16 15:04:09 +0300

    added stylesheet for splash screen and advanced in styling game screen

--- tavla/routes/index.js
+++ tavla/routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/splash', function(req, res, next) {
   res.render('splash', { defaultImage: '/images/EarthCountryball.png' });
+});
+
+
+/* GET home page. */
+router.get('/game', function (req, res, next) {
+  talks =['fuck you', 'lol', 'asdsad'];
+  res.render('game', 
+  { 
+    talks: talks });
+    
 });
 
 module.exports = router;
