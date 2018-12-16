10fc14fd3ce77d20dc569a49329b3ae11e86c46a
Murat Kaan Meral <muratkaanmeral@gmail.com>
2018-12-16 13:17:32 +0100

    Add config

---
+++ tavla/public/javascripts/config.js
@@ -0,0 +1,7 @@
+
+(function (exports) {
+
+    exports.WEB_SOCKET_URL = "ws://localhost:3000"; /* WebSocket URL */
+    exports.DEFAULT_BOARD = [6,6,6,6,6,8,8,8,13,13,13,13,13,24,24];
+
+}(typeof exports === "undefined" ? this.Setup = {} : exports));
